ba84556e0eaa2ce81bfb3d9297ed6edf1ae8a800
michelle <michelle.graham@dkit.ie>
2023-03-28 22:55:56 +0100

    Testing commit conflict resolution

--- Maps/src/main/java/utils/HashMap.java
+++ Maps/src/main/java/utils/HashMap.java
@@ -5,7 +5,7 @@ package utils;
  * @author michelle
  */
 public class HashMap {
-    private static final int DEFAULT_CAPACITY = 20;
+    private static final int DEFAULT_CAPACITY = 23;
     private Entry [] data;
     private int size;
     
@@ -24,27 +24,27 @@ public class HashMap {
         return size;
     }
     
-    private int hashFunction(String key){
+    private int calculateSlot(String key){
         int hash = key.hashCode();
         hash = Math.abs(hash) % data.length;
         return hash;
     }
     
     public String put(String key, String value){
-        int calculatedSlot = hashFunction(key);
-        if(data[calculatedSlot] == null){
+        int slot = calculateSlot(key);
+        if(data[slot] == null){
             Entry newEntry = new Entry(key, value);
-            data[calculatedSlot] = newEntry;
+            data[slot] = newEntry;
             size++;
             return null;
         }else{
-            String oldValue = data[calculatedSlot].updateValue(value);
+            String oldValue = data[slot].updateValue(value);
             return oldValue;
         }
     }
     
     private static class Entry{
-        private String key;
+        private final String key;
         private String value;
         
         public Entry(String key, String value){
